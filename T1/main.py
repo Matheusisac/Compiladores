1d832c4d6b2ba377059498a8b70457190b76c0cd
Matheusisac <45320600+Matheusisac@users.noreply.github.com>
2023-05-23 15:26:29 -0300

    Update main.py

--- T1/main.py
+++ T1/main.py
@@ -1,51 +1,50 @@
-import os
 import string
-import pandas as pd
 
-col = 0
-linha = 0
-estado = 0
-palavra = []
-listlinha = []
-listcol = []
-lexemalist = []
-estadolist = []
-tipolist = []
-classelist = []
-erros = []
+class Lexico:
+    global numeros, letras, reservadas, estados_finais
 
-
-#Tabela de Simbolos
-
-estados_finais = {
-    1: ('Num', None, 'inteiro'),
-    3: ('Num', None, 'real'),
-    6: ('Num', None, 'real'),
-    8: ('Lit', None, None),
-    9: ('id', None, None),
-    11: ('comentario', None, None),
-    12: ('EOF', None, None),
-    13: ('OPR', None, None),
-    14: ('OPR', None, None),
-    15: ('OPR', None, None),
-    16: ('RCB', None, None),
-    17: ('OPR', None, None),
-    18: ('OPR', None, None),
-    19: ('OPR', None, None),
-    20: ('AB_P', None, None),
-    21: ('FC_P', None, None),
-    22: ('PT_V', None, None),
-    23: ('VIR', None, None),
-    24: ('OPM', None, None),
-}
-letras = list(string.ascii_letters)
-numeros = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-reservadas = ['inicio', 'varinicio', 'varfim', 'escreva', 'leia', 'se', 'entao', 'fimse', 'Repita', 'fimRepita', 'fim', 'inteiro', 'literal', 'real']
+    #Tabela de Simbolos
+    estados_finais = {
+        1: ('Num', None, 'inteiro'),
+        3: ('Num', None, 'real'),
+        6: ('Num', None, 'real'),
+        8: ('Lit', None, None),
+        9: ('id', None, None),
+        11: ('comentario', None, None),
+        12: ('EOF', None, None),
+        13: ('OPR', None, None),
+        14: ('OPR', None, None),
+        15: ('OPR', None, None),
+        16: ('RCB', None, None),
+        17: ('OPR', None, None),
+        18: ('OPR', None, None),
+        19: ('OPR', None, None),
+        20: ('AB_P', None, None),
+        21: ('FC_P', None, None),
+        22: ('PT_V', None, None),
+        23: ('VIR', None, None),
+        24: ('OPM', None, None),
+    }
+    letras = list(string.ascii_letters)
+    numeros = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    reservadas = ['inicio', 'varinicio', 'varfim', 'escreva', 'leia', 'se', 'entao', 'fimse', 'Repita', 'fimRepita', 'fim', 'inteiro', 'literal', 'real']
 
 
-#Scanner
+    #Scanner
+    def Scanner(fonte, err = False):
+        col = 0
+        linha = 0
+        estado = 0
+        palavra = []
+        listlinha = []
+        listcol = []
+        lexemalist = []
+        estadolist = []
+        tipolist = []
+        classelist = []
+        erros = []
 
-with open('D:/Compiladores-main/Compiladores-main/T1/FONTE.ALG', 'r') as file:
+        with open(fonte, 'r') as file:
             code = file.read()
             i = 0
             while i < len(code):
@@ -53,7 +52,7 @@ with open('D:/Compiladores-main/Compiladores-main/T1/FONTE.ALG', 'r') as file:
                 palavra.append(code[i])
                 if(code[i] == '\n'):
                     col = 0
-                    linha += 1
+                    linha = linha + 1
                 match(estado):
                     case 13:
                         if(code[i] == '='):
@@ -270,31 +269,27 @@ with open('D:/Compiladores-main/Compiladores-main/T1/FONTE.ALG', 'r') as file:
             lexemalist.append('EOF')
             listcol.append(col)
             listlinha.append(linha)
+        
+        for i in range(len(lexemalist)):
+            if(lexemalist[i] in reservadas):
+                classelist.append(lexemalist[i])
+                tipolist. append(lexemalist[i])
+            else:
+                try:
+                    classelist.append(estados_finais[estadolist[i]][0])
+                    tipolist.append(estados_finais[estadolist[i]][2])
+                except:
+                    erros.append(f"ERRO léxico – Caractere inválido na linguagem. Linha {listlinha[i]} e Coluna {listcol[i]}")
+                    classelist.append("ERRO")
+                    tipolist.append("None")
 
-for i in range(len(lexemalist)):
-    if(lexemalist[i] in reservadas):
-        classelist.append(lexemalist[i])
-        tipolist. append(lexemalist[i])
-    else:
-        try:
-            classelist.append(estados_finais[estadolist[i]][0])
-            tipolist.append(estados_finais[estadolist[i]][2])
-        except:
-            erros.append(f"ERRO léxico – Caractere inválido na linguagem. Linha {listlinha[i]} e Coluna {listcol[i]}")
-            classelist.append("ERRO")
-            tipolist.append("None")
-
-Token = {
-    'Classe': classelist,
-    'Lexema': lexemalist,
-    'Tipo': tipolist 
-}
-
-tabela = pd.DataFrame(Token)
-print(tabela.to_string())
-#for o in range(len(lexemalist)):
-#    print(tabela.loc[o])
-#    print(Token['Classe'][o])
-
-for n in erros:
-    print('\n' + n)
+        if(err == False):
+            Token = {
+                'Classe': classelist,
+                'Lexema': lexemalist,
+                'Tipo': tipolist 
+            }
+            return Token
+        else:
+            return erros
+            
