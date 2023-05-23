76d0326c6cdb9d39218e3d175c8437c824c491de
Matheusisac <45320600+Matheusisac@users.noreply.github.com>
2023-05-23 15:27:23 -0300

    Add files via upload

---
+++ T1/main.py
@@ -0,0 +1,10 @@
+from lexico import Lexico
+import pandas as pd
+
+var = Lexico.Scanner('D:/Compiladores-main/Compiladores-main/T1/FONTE.ALG')
+
+df = pd.DataFrame(var)
+
+lexema = df.loc[0]['Lexema']
+
+print(lexema)
