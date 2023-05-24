ba27da4b6a57b5e398f3b316856fbf23ba199e4d
Matheusisac <45320600+Matheusisac@users.noreply.github.com>
2023-05-24 12:52:07 -0300

    Update main.py

--- T1/main.py
+++ T1/main.py
@@ -2,9 +2,15 @@ from lexico import Lexico
 import pandas as pd
 
 var = Lexico.Scanner('D:/Compiladores-main/Compiladores-main/T1/FONTE.ALG')
+erros = Lexico.Scanner('D:/Compiladores-main/Compiladores-main/T1/FONTE.ALG', True)
 
+index = 0
 df = pd.DataFrame(var)
 
-lexema = df.loc[0]['Lexema']
-
-print(df.to_string())
+for i in range(len(var['Classe'])):
+    lexema = df.loc[i]
+    print(lexema)
+    if(df.loc[i]['Classe'] == 'ERRO'):
+        print(erros[index])
+        index += 1
+    print('\n')
